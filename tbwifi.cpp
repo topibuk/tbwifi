cbdcfd4ec22b933aadec100dfa1a5077a104f7db
Victor Abramov <vicabramov@gmail.com>
2021-08-16 16:10:11 +0300

    implement worker thread

--- tbwifi.cpp
+++ tbwifi.cpp
@@ -7,7 +7,17 @@ TBWiFi &TBWiFi::getInstance()
 	return instance;
 }
 
+void TBWiFi::task(void *v)
+{
+	while (true)
+	{
+		ESP_LOGV(LOG_TAG, "heartbeat");
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+}
+
 TBWiFi::TBWiFi()
 {
 	ESP_LOGV(LOG_TAG, "tbwifi constructor called");
+	xTaskCreate(TBWiFi::task, "TBWiFi_task", 4096, NULL, 10, NULL);
 }
--- tbwifi.hpp
+++ tbwifi.hpp
@@ -14,6 +14,7 @@ class TBWiFi
 {
 private:
 	TBWiFi();
+	static void task(void *v);
 
 public:
 	static TBWiFi &getInstance();
